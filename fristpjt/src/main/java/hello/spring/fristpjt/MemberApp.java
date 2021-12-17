57ac63c7c67fcfa9fee50adf619eae0ef7815585
Nablekim94 <Nablekim94@gmail.com>
2021-12-17 21:41:13 +0900

    main 함수로 '회원 도메인 설계 코드' 테스트하기

---
+++ fristpjt/src/main/java/hello/spring/fristpjt/MemberApp.java
@@ -0,0 +1,21 @@
+package hello.spring.fristpjt;
+
+import hello.spring.fristpjt.member.Grade;
+import hello.spring.fristpjt.member.Member;
+import hello.spring.fristpjt.member.MemberService;
+import hello.spring.fristpjt.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "seungsoo", Grade.VIP);
+
+        // 회원 가입 시키기
+        memberService.join((member));
+
+        // 회원 가입한 정보와 조회한 정보가 일치하는지 확인
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find member = " + findMember.getName());
+    }
+}
