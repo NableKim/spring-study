676cc22b4b37369193b7ea64d16733842a197ef7
nablekim94 <nablekim94@gmail.com>
2021-12-17 16:28:58 +0900

    Create initial project

---
+++ fristpjt/src/test/java/hello/spring/fristpjt/FristpjtApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.spring.fristpjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FristpjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
