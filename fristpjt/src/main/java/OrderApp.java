e87c59e75a884a442f5694767526f8908fa7c8d1
Nablekim94 <Nablekim94@gmail.com>
2021-12-20 00:17:04 +0900

    main 함수로 '주문, 할인 도메인 설계 코드' 테스트하기

---
+++ fristpjt/src/main/java/OrderApp.java
@@ -0,0 +1,24 @@
+import hello.spring.fristpjt.member.Grade;
+import hello.spring.fristpjt.member.Member;
+import hello.spring.fristpjt.member.MemberService;
+import hello.spring.fristpjt.member.MemberServiceImpl;
+import hello.spring.fristpjt.order.Order;
+import hello.spring.fristpjt.order.OrderService;
+import hello.spring.fristpjt.order.OrderServiceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "seungsoo", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "iPhone13", 1500000);
+        System.out.println("order = " + order);
+        System.out.println("discount price = " +order.calculatePrice());
+    }
+
+}
