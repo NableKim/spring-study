7aeba050e8447725be5e219256decda2ff91006e
nablekim94 <nablekim94@gmail.com>
2022-01-19 13:15:07 +0900

    spring framework 적용 - ApplicationContext

--- fristpjt/src/main/java/hello/spring/fristpjt/AppConfig.java
+++ fristpjt/src/main/java/hello/spring/fristpjt/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.spring.fristpjt;
 
 import hello.spring.fristpjt.discount.DiscountPolicy;
-import hello.spring.fristpjt.discount.FixDiscountPolicy;
 import hello.spring.fristpjt.discount.RateDiscountPolicy;
 import hello.spring.fristpjt.member.MemberRepository;
 import hello.spring.fristpjt.member.MemberService;
@@ -9,22 +8,29 @@ import hello.spring.fristpjt.member.MemberServiceImpl;
 import hello.spring.fristpjt.member.MemoryMemberRepository;
 import hello.spring.fristpjt.order.OrderService;
 import hello.spring.fristpjt.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
--- fristpjt/src/main/java/hello/spring/fristpjt/MemberApp.java
+++ fristpjt/src/main/java/hello/spring/fristpjt/MemberApp.java
@@ -4,11 +4,16 @@ import hello.spring.fristpjt.member.Grade;
 import hello.spring.fristpjt.member.Member;
 import hello.spring.fristpjt.member.MemberService;
 import hello.spring.fristpjt.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        //AppConfig appConfig = new AppConfig();
+        // MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         Member member = new Member(1L, "seungsoo", Grade.VIP);
 
         // 회원 가입 시키기
