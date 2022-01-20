1f5d8f4fbe75a11b4ee710664441fedd824c558d
nablekim94 <nablekim94@gmail.com>
2022-01-20 21:41:37 +0900

    모든 스프링 빈의 BeanDefinition 정보 출력하기

---
+++ fristpjt/src/test/java/hello/spring/fristpjt/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,28 @@
+package hello.spring.fristpjt.beandefinition;
+
+import hello.spring.fristpjt.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanDefinitionTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("BeanDefinition 정보 출력하기")
+    void findApplicationBean() {
+
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName="+beanDefinitionName +
+                        " beanDefinition=" + beanDefinition);
+            }
+        }
+    }
+}
