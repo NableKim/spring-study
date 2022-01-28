5f3adad5c14fbcc054d8cf92a9e75293b5915658
nablekim94 <nablekim94@gmail.com>
2022-01-28 22:24:03 +0900

    request scope와 ObjectProvider 사용 예제

---
+++ fristpjt/src/main/java/hello/spring/fristpjt/logdemo/LogDemoService.java
@@ -0,0 +1,18 @@
+package hello.spring.fristpjt.logdemo;
+
+import hello.spring.fristpjt.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+
+    public void logic(String testId) {
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
+        myLogger.log("testId = " + testId);
+    }
+}
---
+++ fristpjt/src/main/java/hello/spring/fristpjt/web/LogDemoController.java
@@ -0,0 +1,36 @@
+package hello.spring.fristpjt.web;
+
+import hello.spring.fristpjt.common.MyLogger;
+import hello.spring.fristpjt.logdemo.LogDemoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        
+        // Log 찍기
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
+        myLogger.setRequestURL(requestURL);
+        myLogger.log("controller test");
+        
+        // Service 로직 실행
+        logDemoService.logic("testId");
+        return "OK";
+    }
+
+
+}
