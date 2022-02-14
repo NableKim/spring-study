f6d6a9a16e39261fbf4e81713c733cdcc2d46bfa
nablekim94 <nablekim94@gmail.com>
2022-02-14 14:04:05 +0900

    Spring MVC 강좌 학습을 위한 Repository 생성

---
+++ springmvc/src/test/java/seungsoo/springmvc/SpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package seungsoo.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
