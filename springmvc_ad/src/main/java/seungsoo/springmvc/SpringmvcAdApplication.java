a6622f152deb9d301857585fd4b2c6ba62875944
nablekim94 <nablekim94@gmail.com>
2022-02-20 15:23:01 +0900

    본격적으로 Spirng MVC 기능을 살펴볼 프로젝트 생성

---
+++ springmvc_ad/src/main/java/seungsoo/springmvc/SpringmvcAdApplication.java
@@ -0,0 +1,13 @@
+package seungsoo.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcAdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcAdApplication.class, args);
+	}
+
+}
